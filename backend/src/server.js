47377491e6f145c573fe60490235e30ec5217644
Rricardo687 <100647749+Rricardo687@users.noreply.github.com>
2022-04-26 20:31:51 -0500

    creando tareas crud

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const todosRouter = require('./routes/todos');
+
+
+
+const server = express();
+const port = 4000;
+server.use(express.json());
+server.use('/api/v1',todosRouter);
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch((err) =>
+    console.log('Error al conectar a la base de datos, error: ' + err)
+  );
+
+server.get('/', (request, response) => {
+    response.send('Hola desde la raíz');
+  });
+
+  server.use('/api/v1',todosRouter);
+
+
+
+  server.listen(port, () => {
+    console.log(`Servidor corriendo en localhost, en el puerto ${port}`);
+  });
